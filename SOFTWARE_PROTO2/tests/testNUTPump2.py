56bd5a82aab0267d0ab1f31acd7c4142eb2b0d92
Antonin <antonin.renaudier@gmail.com>
2019-01-30 18:25:07 +0100

    Debug des codes

---
+++ SOFTWARE_PROTO2/tests/testNUTPump2.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+pin=29
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
+
+GPIO.output(pin, GPIO.HIGH)
+
+time.sleep(15)
+
+GPIO.output(pin, GPIO.LOW)
+
+GPIO.cleanup()
