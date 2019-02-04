1ad686260769273aaf31636c7bd89d1e98b11fb1
AntoinePinaud <40756085+AntoinePinaud@users.noreply.github.com>
2019-02-04 15:27:32 +0100

    Ajout test capteurs

--- SOFTWARE_PROTO2/tests/testEC.py
+++ SOFTWARE_PROTO2/tests/testEC.py
@@ -1,7 +1,8 @@
 import RPI.GPIO as GPIO
+from AtlasI2C import *
 
-EC_I2C_address= ...
+EC_I2C_address= Ox64
 #get EC value
 device = AtlasI2C(EC_I2C_address)     # creates the I2C port object, specify the address or bus if necessary
-EC = info = string.split(device.query("I"), ",")[1]
+EC = string.split(device.query("R"), ",")
 print(EC)
---
+++ SOFTWARE_PROTO2/tests/test_SENSORS.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+from AtlasI2C import *
+import time
+import board
+import busio
+import adafruit_am2320
+from ds18b20 import *
+
+#ini pH
+pH_I2C_address = 0x63
+
+# ini EC
+EC_I2C_address= 0x64
+
+#ini ATM_T\H
+i2c = busio.I2C(board.SCL, board.SDA)
+am = adafruit_am2320.AM2320(i2c)
+
+#ini WAR_T
+ds18b20 = DS18B20()
+
+#ini WAR_level
+###############
+
+while True :
+    #Get pH value
+    device = AtlasI2C(pH_I2C_address)     # creates the I2C port object, specify the address or bus if necessary
+    pH = string.split(device.query("R"), ",")
+    print(pH)
+    time.sleep(10)
+
+    # Get EC value
+    device = AtlasI2C(EC_I2C_address)     # creates the I2C port object, specify the address or bus if necessary
+    EC = string.split(device.query("R"), ",")
+    print(EC)
+    time.sleep(10)
+
+    #Get ATM_T\H value
+    print("Temperature: ", am.temperature)
+    print("Humidity: ", am.relative_humidity)
+    time.sleep(10)
+
+    # Get WAR_T
+    ds18b20.read_temp()
+    time.sleep(10)
+
+    # Get WAR_level
+    ###############
+    time.sleep(10)
