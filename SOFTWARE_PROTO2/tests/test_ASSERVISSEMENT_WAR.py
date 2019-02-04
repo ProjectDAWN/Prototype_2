7b8040b195fc4a37ecc83d93a3e8110739d717a4
AntoinePinaud <40756085+AntoinePinaud@users.noreply.github.com>
2019-02-04 18:20:47 +0100

    Ajout Tests Asservissement

---
+++ SOFTWARE_PROTO2/tests/test_ASSERVISSEMENT_WAR.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+import datetime
+
+GPIO.setmode(GPIO.BOARD)
+
+##########################################################################
+#TEST WATERING Module
+
+while True :
+    
