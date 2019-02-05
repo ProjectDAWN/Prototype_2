ba01193d5a4ac55d7c7f5c5b1bb1851b439a6da7
AntoinePinaud <40756085+AntoinePinaud@users.noreply.github.com>
2019-02-05 14:47:59 +0100

    Modif cal

--- SOFTWARE_PROTO2/tests/CalibrationpH.py
+++ SOFTWARE_PROTO2/tests/CalibrationpH.py
@@ -2,7 +2,17 @@ import RPi.GPIO as GPIO
 from AtlasI2C import *
 
 pH_I2C_address = 0x63
-#get pH value
+
 device = AtlasI2C(pH_I2C_address)     # creates the I2C port object, specify the address or bus if necessary
-pH = string.split(device.query("Cal"), ",")
+
+# Step 1
+pH = string.split(device.query("Cal,mid,7.01"), ",")
 print(pH)
+
+# # Step 2
+# pH = string.split(device.query("Cal,low,4.01"), ",")
+# print(pH)
+#
+# # Step 3
+# pH = string.split(device.query("Cal,high,10.01"), ",")
+# print(pH)
