093ed14b68c928d352049a412c69d32af78e55ea
AntoinePinaud <40756085+AntoinePinaud@users.noreply.github.com>
2019-02-05 14:36:18 +0100

    Ajout calibration pH

---
+++ SOFTWARE_PROTO2/tests/CalibrationpH.py
@@ -0,0 +1,8 @@
+import RPi.GPIO as GPIO
+from AtlasI2C import *
+
+pH_I2C_address = 0x63
+#get pH value
+device = AtlasI2C(pH_I2C_address)     # creates the I2C port object, specify the address or bus if necessary
+pH = string.split(device.query("Cal"), ",")
+print(pH)
---
+++ SOFTWARE_PROTO2/tests/test_ASSERVISSEMENT_NUT.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import datetime
+
+GPIO.setmode(GPIO.BOARD)
+
+##########################################################################
+#TEST NUTRIENTS Module
+
+while True :
+
+    
