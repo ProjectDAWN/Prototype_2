5a56f747be7263ad608b98ac41ec8be9a5033d91
MatthiasRamos <matthias.ramos@yahoo.fr>
2019-02-12 17:06:20 +0100

    feature : GPIO_sensors

--- SOFTWARE_PROTO2/Raspberry_Interface/GPIO_Sensors.py
+++ SOFTWARE_PROTO2/Raspberry_Interface/GPIO_Sensors.py
@@ -2,14 +2,15 @@
 import sys
 sys.path.append(sys.path[0]+"/..")
 from Data_Managers.Reads_Writes.CSV_reader import CSV_reader
-from sensor_classes import *
+from Raspberry_Interface.sensor_classes import *
 
 class GPIO_Sensors :
+
+    class_dict = {"nom_capteur" : class_capteur}
 
     def __init__(self,pin_file,realMode=True) :
         self.sensors = CSV_reader(pin_file) #instantiation of the actuators manager class
         self.nb_pins = self.actuators.nb_index
-        self.pins_dict = dict(zip(self.sensors.get_list("pin"),[None]*self.nb_pins)) #dict of state by pin
         self.realMode = realMode
         if self.realMode:
             import RPi.GPIO as GPIO
@@ -19,8 +20,8 @@ class GPIO_Sensors :
         """this function check error on pin activation
         take a pin and a bool indicating the info to communicate"""
 
-        if(pin not in self.pins_dict.keys()):
-            print("Pin {} doesn't exist".format(pin))
+        if(pin not in GPIO_Sensors.class_dict.keys()):
+            print("captor {} doesn't exist".format(pin))
             return(False)
         else:
             return(True)
@@ -28,16 +29,16 @@ class GPIO_Sensors :
     def read(self,pin):
         """Given pins or sensor,
         check if the activation is possible and activate it"""
-        for pin in actuators: #actuators is a list of string representing actuators
-            if(not isinstance(pin,int)):
-                pin = self.sensors.get(pin,"pin") # allow to the user (main) to choose between pin or actuator name
-            if self.verif_pin(pin,True):
+        if(not isinstance(pin,int)):
+            pin = self.sensors.get(pin,"pin") # allow to the user (main) to choose between pin or actuator name
+        if self.verif_pin(pin,True):
+            self.pins_dict[pin]=-1
+            if(self.realMode):
+                class = GPIO_Sensors.class_dict[nom_capteur]
                 self.pins_dict[pin]=-1
-                if(self.realMode):
-                    self.pins_dict[pin]=-1
 
-In = GPIO_Sensors("../Files/Actuators.csv",False)
-In.read("NUT_Mixer")
+#In = GPIO_Sensors("../Files/Actuators.csv",False)
+#In.read("NUT_Mixer")
 
-In.read(11)
-In.cleanup()
+#In.read(11)
+#In.cleanup()
