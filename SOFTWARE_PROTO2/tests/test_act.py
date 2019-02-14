820bb189f7551fba2ebcb7fc749e1705bcccc501
AntoinePinaud <40756085+AntoinePinaud@users.noreply.github.com>
2019-02-14 14:28:14 +0100

    test : test_act

--- SOFTWARE_PROTO2/tests/test_act.py
+++ SOFTWARE_PROTO2/tests/test_act.py
@@ -4,12 +4,14 @@ sys.path.append(path)
 from Raspberry_Interface.GPIO_Actuators import GPIO_Actuators
 pin_file = path + "/Files/Actuators.csv"
 realMode = False
-import time 
+import time
 InOut = GPIO_Actuators(pin_file,realMode)
 
 def test(actuator):
 	InOut.activate(actuator)
 	print("Allumé")
-	time.sleep(5)
+	time.sleep(20)
 	InOut.desactivate(actuator)
 	print("Eteint")
+
+test("NUT_Pump_MATO")
