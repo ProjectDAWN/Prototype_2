c58e19c2d1a1f7fb7be223ec3ec99ad3c66ee963
MatthiasRamos <matthias.ramos@yahoo.fr>
2019-02-14 16:56:09 +0100

    feature : stop programm

--- SOFTWARE_PROTO2/Terminate_programm/Stop.py
+++ SOFTWARE_PROTO2/Terminate_programm/Stop.py
@@ -1,55 +1,11 @@
-GPIO.setmode(GPIO.BOARD)
+from Raspberry_Interface import GPIO_Actuators, GPIO_Sensors
+import time
 
-####### Atmospheric module (ATM)
-ATMventilator_pin =  ...
-GPIO.setup(ATMventilator_pin, GPIO.OUT, initial = GPIO.LOW)
-ATMmistmaker_pin = ...
-GPIO.setup(ATMmistmaker_pin, GPIO.OUT, initial = GPIO.LOW)
-ATMelectricwarmer_pin = ...
-GPIO.setup(ATMelectricwarmer_pin, GPIO.OUT, initial = GPIO.LOW)
+InOutMode = "GPIO"
+chanel_file = "Files/Actuators.csv"
 
-####### Lighting module (LIG)
-LIGled_pin = ...
-GPIO.setup(LIGled_pin, GPIO.OUT, initial = GPIO.LOW)
-####### Nutrients module (NUT)
-NUTpump1_pin = ...                     #Flora Micro/Mato
-GPIO.setup(NUTpump1_pin, GPIO.OUT, initial = GPIO.LOW)
-NUTpump2_pin = ...                     #FloraGro
-GPIO.setup(NUTpump2_pin, GPIO.OUT, initial = GPIO.LOW)
-NUTpump3_pin = ...                     #FloraBloom
-GPIO.setup(NUTpump3_pin, GPIO.OUT, initial = GPIO.LOW)
-size_x_bac = ...
-size_y_bac ...
-####### Watering module (WAT)
-WARultrasonicmistmaker_pin = ...
-GPIO.setup(WARultrasonicmistmaker_pin, GPIO.OUT, initial = GPIO.LOW)
-WARmixer_pin = ...
-GPIO.setup(WARmixer_pin, GPIO.OUT, initial = GPIO.LOW)
-WARventilator_pin = ...
-GPIO.setup(WARventilator_pin, GPIO.OUT, initial = GPIO.LOW)
-WARwaterlevel_pin = ...
-WARpHup_pin = ...
-GPIO.setup(WARpHup_pin, GPIO.OUT, initial = GPIO.LOW)
-WARpHdown_pin = ...
-GPIO.setup(WARpHdown_pin, GPIO.OUT, initial = GPIO.LOW)
-pH_I2C_adress = ...
-EC_I2C_address = ...
-
-"put all the GPIO pins at LOW value"
-#ATM module
-GPIO.output(ATMventilator_pin, GPIO.LOW)
-GPIO.output(ATMmistmaker_pin, GPIO.LOW)
-GPIO.output(ATMelectricwarmer_pin, GPIO.LOW)
-#LIG module
-GPIO.output(LIGled_pin, GPIO.LOW)
-#NUT module
-GPIO.output(NUTpump1_pin, GPIO.LOW)
-GPIO.output(NUTpump2_pin, GPIO.LOW)
-GPIO.output(NUTpump3_pin, GPIO.LOW)
-#WAT module
-GPIO.output(WARultrasonicmistmaker_pin, GPIO.LOW)
-GPIO.output(WARmixer_pin, GPIO.LOW)
-GPIO.output(WARventilator_pin, GPIO.LOW)
-GPIO.output(WARwatermevel_pin, GPIO.LOW)
-GPIO.output(WARpHup_pin, GPIO.LOW)
-GPIO.output(WARpHdown_pin, GPIO.LOW)
+actuators = GPIO_Actuators.GPIO_Actuators(chanel_file,InOutMode)
+actuators.cleanup()
+time.sleep(25)
+actuators.activate("LIG_Led")
+print("Stopped !")
