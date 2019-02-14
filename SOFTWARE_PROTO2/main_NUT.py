4d4b60e1e0eb83f54a8f69230690254522eb10dc
MatthiasRamos <matthias.ramos@yahoo.fr>
2019-02-14 14:15:00 +0100

    Debug Raspberry

--- SOFTWARE_PROTO2/main_NUT.py
+++ SOFTWARE_PROTO2/main_NUT.py
@@ -75,9 +75,9 @@ def nutrients_loop(day,climate_recipe):
 
 def end_loop():
     """put all the actuators pins at LOW value"""
-    actuators.desactivate(NUT_Pump_pHDown,
-                    NUT_Pump2,
-                    NUT_Pump3)
+    actuators.desactivate("NUT_Pump_pHDown",
+                    "NUT_Pump2",
+                    "NUT_Pump3")
 
 date_current = datetime.datetime.now()
 diff = datetime.datetime.now() - date_ini
