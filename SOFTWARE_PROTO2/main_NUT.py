d75fc1a52373de6d66b8d12a19a59f349c768e07
MatthiasRamos <matthias.ramos@yahoo.fr>
2019-02-14 13:54:05 +0100

    Debug : NUT

--- SOFTWARE_PROTO2/main_NUT.py
+++ SOFTWARE_PROTO2/main_NUT.py
@@ -36,42 +36,44 @@ from Raspberry_Interface.sensor_classes import AtlasI2C
 pin_file = "Files/Actuators.csv"
 realMode = False
 variety = "tomato"
+InOutMode = "GPIO"
 date_file = open("Files/date_ini",'rb')
 depickler = pickle.Unpickler(date_file)
 date_ini = depickler.load()
-InOut = GPIO_Actuators.GPIO_Actuators(pin_file,realMode)
+actuators = GPIO_Actuators.GPIO_Actuators(pin_file,InOutMode,realMode)
+sensors = GPIO_Sensors.GPIO_Sensors(InOutMode,realMode)
 climate_recipe = Climate_recipe(variety)
 
 size_x_bac = 80 #cm
 size_y_bac = 50 #cm
-water_level = GPIO_Sensors.read("waterlevel")
 flow= 1.6 #pump's flow = 1.6ml.s-1
-volume = size_x_bac*size_y_bac*water_level
-coeff = volume/3.79 #3.79 --> conversion Litre/Gallons
-
+water_level = sensors.read("waterlevel")
+volume = size_x_bac*size_y_bac*water_level/1000
+coef = volume/flow
 
 
 ####### Nutrients module
 def nutrients_loop(day,climate_recipe):
     """nutrients_loop is a function that control the release of nutrients according to climate recipe"""
     FloraMicro = climate_recipe.floraMicro(day) #ml
-    InOut.activate("NUT_Pump_pHDown")
-    time.sleep(FloraMicro/flow*coeff)
-    InOut.desactivate("NUT_Pump_pHDown")
+    actuators.activate("NUT_Pump_Micro")
+    time.sleep(FloraMicro*coef)
+    print(FloraMicro*coef)
+    actuators.desactivate("NUT_Pump_Micro")
     FloraGro = climate_recipe.floraGro(day) #ml
-    InOut.activate("NUT_Pump_BioGrow")
-    time.sleep(FloraGro/flow*coeff)
-    InOut.desactivate("NUT_Pump_BioGrow")
+    actuators.activate("NUT_Pump_BioGro")
+    time.sleep(FloraGro*coef)
+    actuators.desactivate("NUT_Pump_BioGro")
     FloraBloom = climate_recipe.floraBloom(day) # ml
-    InOut.activate("NUT_Pump_BioBloom")
-    time.sleep(FloraBloom/flow*coeff)
-    InOut.desactivate("NUT_Pump_BioBloom")
+    actuators.activate("NUT_Pump_BioBloom")
+    time.sleep(FloraBloom*coef)
+    actuators.desactivate("NUT_Pump_BioBloom")
 
 ####### End of growth
 
 def end_loop():
-    """put all the InOut pins at LOW value"""
-    InOut.desactivate(NUT_Pump_pHDown,
+    """put all the actuators pins at LOW value"""
+    actuators.desactivate(NUT_Pump_pHDown,
                     NUT_Pump2,
                     NUT_Pump3)
 
