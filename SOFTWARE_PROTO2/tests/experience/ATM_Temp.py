c7fc7ad7a2cb3ca27841c7191f348edfd856198f
Antonin Renaudier <antonin.renaudier@gmail.com>
2019-02-14 18:37:19 +0100

    Refactor: experience atm_hum

--- SOFTWARE_PROTO2/tests/experience/ATM_Temp.py
+++ SOFTWARE_PROTO2/tests/experience/ATM_Temp.py
@@ -1,4 +1,5 @@
-import matplotlib.pyplot
+import matplotlib.pyplot as plt 
+import nummpy as np
 import sys
 path = sys.path[0]+"/../.."
 sys.path.append(path)
@@ -6,27 +7,37 @@ from Raspberry_Interface.GPIO_Actuators import GPIO_Actuators
 from Raspberry_Interface.GPIO_Sensors import GPIO_Sensors
 pin_file = path + "/Files/Actuators.csv"
 InOutMode = "GPIO"
-realMode = False
+realMode = True
 import time
 InOut = GPIO_Actuators(pin_file,InOutMode,realMode)
 sensors = GPIO_Sensors(InOutMode,realMode)
 
 def test(actuator):
 	values=[]
-	ti = [x for x in range(0,180,10)]
+	ti = np.linspace(0,300,300)
+
+	t = time.time()
+	t_empty = t + 60
+
+	print("Test sans actionneurs")
+	while time.time() < t_empty :
+		values.append(sensors.read("temperature"))
+		time.sleep(1)
+
 	InOut.activate(actuator)
 	print("Allumé")
 
-	t = time.time()
-	t_fin = t + 180
-
+	t_fin = time.time() + 180
+	print("Test avec actionneurs")
 	while time.time() < t_fin :
-		values.append(sensors.read("pH"))
-		time.sleep(10)
+		values.append(sensors.read("temperature"))
+		time.sleep(1)
 
 	InOut.desactivate(actuator)
 	print("Eteint")
 
-	plot(values, ti)
+	plt.grid()
+	plt.plot(ti,values)
+	plt.show()
 
 test("ATM_Warmer")
