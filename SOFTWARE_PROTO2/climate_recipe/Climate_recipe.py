cb03d5bd0787ada8abc170ac4bf5617a0641e0d8
MatthiasRamos <matthias.ramos@yahoo.fr>
2019-02-19 18:35:09 +0100

    debug : Raspberry

--- SOFTWARE_PROTO2/climate_recipe/Climate_recipe.py
+++ SOFTWARE_PROTO2/climate_recipe/Climate_recipe.py
@@ -78,7 +78,7 @@ class Climate_recipe:
         """return the coef in s/ml of the pump depending on the water_level"""
         size_tank_x,size_tank_y = self.system["size_tank_x"],self.system["size_tank_y"] #cm
         flow= self.system["flux_NUT_Pump_"+pump] #pump's flow
-        volume = size_tank_x*size_tank_y*water_level/1000
+        volume = size_tank_x*size_tank_y*water_level/10000
         coef = volume/flow
         return(coef)
 
