333f55cc58fd023d3c5574b469c9b0f085c29138
Antonin Renaudier <antonin.renaudier@gmail.com>
2019-02-19 16:46:30 +0100

    Refactor: Sensor test

--- SOFTWARE_PROTO2/tests/experience/NUT_Pump.py
+++ SOFTWARE_PROTO2/tests/experience/NUT_Pump.py
@@ -4,15 +4,15 @@ sys.path.append(path)
 from Raspberry_Interface.GPIO_Actuators import GPIO_Actuators
 pin_file = path + "/Files/Actuators.csv"
 InOutMode = "GPIO"
-realMode = False
+realMode = True
 import time
 InOut = GPIO_Actuators(pin_file,InOutMode,realMode)
 
 def test(actuator):
 	InOut.activate(actuator)
 	print("Allumé")
-	time.sleep(20)
+	time.sleep(12)
 	InOut.desactivate(actuator)
 	print("Eteint")
 
-test("NUT_Pump_Mato")
+test("NUT_Pump_BioGro")
