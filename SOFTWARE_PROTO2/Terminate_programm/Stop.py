ef9ad1e845db2d37c1c9f3ec29d40ad2d43e877c
MatthiasRamos <matthias.ramos@yahoo.fr>
2019-02-21 14:44:58 +0100

    Debug : Stop programm

--- SOFTWARE_PROTO2/Terminate_programm/Stop.py
+++ SOFTWARE_PROTO2/Terminate_programm/Stop.py
@@ -4,7 +4,7 @@ from Raspberry_Interface import GPIO_Actuators, GPIO_Sensors
 import time
 
 InOutMode = "GPIO"
-chanel_file = "Files/Actuators.csv"
+chanel_file = "../Files/Actuators.csv"
 
 actuators = GPIO_Actuators.GPIO_Actuators(chanel_file,InOutMode)
 actuators.cleanup()
