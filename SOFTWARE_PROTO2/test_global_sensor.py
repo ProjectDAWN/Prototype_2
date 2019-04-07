0f276a109817eff0a2c5254fed6be24534024568
AntoinePinaud <40756085+AntoinePinaud@users.noreply.github.com>
2019-04-07 08:50:23 +0200

    Refactor : no water_temperature sensor (globaltest)

--- SOFTWARE_PROTO2/test_global_sensor.py
+++ SOFTWARE_PROTO2/test_global_sensor.py
@@ -1,4 +1,4 @@
 from test_sensor import test
-sensor_list = ["pH","conductivity","water_level","water_temperature","temperature","humidity"]
+sensor_list = ["pH","conductivity","water_level","temperature","humidity"]
 for sensor in sensor_list:
 	test(sensor)
