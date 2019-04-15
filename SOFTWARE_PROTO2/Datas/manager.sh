345112019f5d5a9a5be330fe201c405ee31dc079
Antoine Pinaud <antoinepm.pinaud@laposte.net>
2019-04-15 13:09:27 +0200

    Feat : script data

---
+++ SOFTWARE_PROTO2/Datas/manager.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#  bash
+#  manager.sh
+#  SOFTWARE_PROTO2
+#  V1.0
+#  DAWN
+#
+###############################################################################
+#
+#                       DATA MANAGER
+#
+###############################################################################
+
+
+d0=`find . -name "20*" |\
+cut -d '/' -f 2 |\
+cut -c-10 |\
+sort -u |\
+tail -n 1`
+echo $d0
+if [ -z "$d0" ]
+then
+  num=3
+else
+  l0=`tail -n 1 $d0.csv`
+  echo $l0
+  num=$(grep -n $l0 $1 | cut -d: -f1 | tail -1)
+  let "num=num+1"
+fi
+
+tail -n +$num $1 | \
+while read line
+do
+  date=`echo $line | cut -d',' -f 1 `
+  echo $line  >> $date.csv
+done
