34d4d01a60e475ebbea22d86673b962de8e3f60b
MatthiasRamos <matthias.ramos@yahoo.fr>
2019-04-19 17:30:06 +0200

    Feature : new methods and optional arguments in CSV_writer

--- SOFTWARE_PROTO2/Data_Managers/Reads_Writes/CSV_writer.py
+++ SOFTWARE_PROTO2/Data_Managers/Reads_Writes/CSV_writer.py
@@ -10,52 +10,66 @@ class CSV_writer:
     technology used to do it (here pandas).
     """
 
-    def __init__(self,file_name):
+    def __init__(self, file_name=None):
         """Constructor of CSV_reader class
 
         file_name -- [string] name of the file to read which contains the datas
         
         """
-        self.df = pd.read_csv(sys.path[0] + "/" + file_name,index_col=0)#think about the case that csv doesn't exist
+        if file_name:
+            self.df = pd.read_csv(sys.path[0] + "/" + file_name, index_col=0)  # think about the case that csv doesn't exist
+        else:
+            self.df = pd.DataFrame()
+
         self.nb_index = self.df.shape[0]
         self.file_name = file_name
 
-    def set(self,id, caracteristic,value):
+    def set(self, key, feature, value):
         """Update one value of the DataFrame
 
         Keyword Arguments:
-        id -- [string or int] the line position
-        caracteristic -- [string] the column position
+        key -- [string or int] the line position
+        feature -- [string] the column position
         value -- [any] the value to put in
 
         """
-        self.df.at[id,caracteristic] = value
+        self.df.at[key, feature] = value
 
-    def set_list(self,caracteristic,list):
+    def set_list(self, feature, list):
         """Update one column of the DataFrame
 
         Keyword Arguments:
-        caracteristic -- [string] the column position
+        feature -- [string] the column position
         list -- [list] the values to put in
 
         """
-        self.df[caracteristic] = pd.DataFrame(list)
+        self.df[feature] = pd.DataFrame(list)
 
-    def set_infos(self,key,infos):
+    def set_infos(self, key, info):
         """Update one line of the DataFrame
 
         Keyword Arguments:
         key -- [string or int] the line position
-        infos -- [any] the value to put in
+        info -- [any] the value to put in
 
         """
-        infos = pd.DataFrame(infos)#change to add key in the df
-        df.update(infos)
+        info = pd.DataFrame(info)  # change to add key in the df
+        self.df.update(info)
 
-    def write(self,file=None):
+    def add(self, other):
+        """Add another dataframe to the existing one
+
+        Keyword Arguments:
+        other -- [pd.DataFrame] the df to add
+
+        """
+        self.df.append(other)
+
+    def write(self, file=None, w_mode='w'):
         """Write the DataFrame in a csv, if file is not given, write in self.file_name
 
         file -- [string] file to write the DataFrame on (default : None)
+        w_mode -- [char] python writing mode (default : 'w', ie "write")
 
         """
         if file:
@@ -63,19 +77,30 @@ class CSV_writer:
         else:
             target = self.file_name
 
-        self.df.to_csv(sys.path[0] + "/" + target)
+        self.df.to_csv(sys.path[0] + "/" + target, mode=w_mode)
 
+    def clear_df(self):
+        """Replace the df by a new empty DataFrame
+
+        Return the old df
+
+        """
+        old_df = self.df
+        self.df = pd.DataFrame()
+        return old_df
+
+    @property
     def txt_to_pandas(file_name):
         """Return DataFrame from a formated .txt file
 
         file_name -- [string] formated .txt file to read
 
         """
-        file = open(file_name,'r')
-        names,items = [],[]
+        file = open(file_name, 'r')
+        names, items = [], []
         for item in file.readlines():
             item = item.split(" ")
             names.append(item[0])
             items.append(int(item[1]))
-        df = pd.DataFrame(pins, index=names, columns = ['pins'])
-        return(df)
+        df = pd.DataFrame(index=names, columns=['pins'])
+        return df
