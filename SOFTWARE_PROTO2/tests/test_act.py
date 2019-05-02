9736541e3f0e2ddb401d5030c33fbee0c46a6cb3
Antonin Renaudier <antonin.renaudier@gmail.com>
2019-05-02 16:28:19 +0200

    Refactor: calibrate pump

--- SOFTWARE_PROTO2/tests/test_act.py
+++ SOFTWARE_PROTO2/tests/test_act.py
@@ -4,15 +4,19 @@ sys.path.append(path)
 from Raspberry_Interface.GPIO_Actuators import GPIO_Actuators
 pin_file = path + "/Files/Actuators.csv"
 InOutMode = "GPIO"
-realMode = False
+realMode = True
 import time
 InOut = GPIO_Actuators(pin_file,InOutMode,realMode)
 
 def test(actuator):
 	InOut.activate(actuator)
 	print("Allumé")
-	time.sleep(20)
+	time.sleep(2)
 	InOut.desactivate(actuator)
 	print("Eteint")
 
 test("NUT_Pump_Mato")
+test("NUT_Pump_pHDown")
+test("NUT_Pump_BioGro")
+test("NUT_Pump_BioBloom")
+test("NUT_Pump_Micro")
